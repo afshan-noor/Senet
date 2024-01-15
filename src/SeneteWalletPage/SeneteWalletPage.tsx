67111e32da702d9e3c1f6a864654d6e6300c9a9e
Afshan10 <afshannoorpk01@gmail.com>
2024-01-15 17:57:04 +0500

    feat: Coming Soon Screen

---
+++ src/SeneteWalletPage/SeneteWalletPage.tsx
@@ -0,0 +1,18 @@
+import Styles from "./SeneteWalletPage.module.css";
+const SeneteWalletPage = () => {
+  return (
+<>
+    <div className={Styles.container}>
+      <div className={Styles.box} >
+      <img src="Senet_logo.png" alt="Senet"></img>
+   <h1>Wallet <span>Creators</span></h1>
+      </div>
+    
+    <p>Coming Soon</p>
+   
+    
+    </div>
+    </>
+  );
+};
+export default SeneteWalletPage;
