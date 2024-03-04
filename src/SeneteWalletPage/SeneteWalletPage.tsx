16cee2cbabdf0aa629b9efeda0c313bf85e93662
Afshan <afshan.noor@ideofuzion.com>
2024-03-04 17:12:43 +0500

    fix: change to nexastream

--- src/SeneteWalletPage/SeneteWalletPage.tsx
+++ src/SeneteWalletPage/SeneteWalletPage.tsx
@@ -4,8 +4,7 @@ const SeneteWalletPage = () => {
 <>
     <div className={Styles.container}>
       <div className={Styles.box} >
-      <img src="Senet_logo.png" alt="Senet"></img>
-   <h1>Wallet <span>Creators</span></h1>
+      <img src="NexaStream.png" alt="Senet"></img>
       </div>
     
     <p>Coming Soon</p>
